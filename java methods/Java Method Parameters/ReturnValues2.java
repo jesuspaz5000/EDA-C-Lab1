01937e1053eeabd2b32266c97d88f69d02770512
jesuspaz <jesuspaz5000>
2023-05-07 13:08:05 -0500

    Return values tercer ejemplo

---
+++ java methods/Java Method Parameters/ReturnValues2.java
@@ -0,0 +1,11 @@
+public class ReturnValues2 {
+    static int myMethod(int x, int y) {
+      return x + y;
+    }
+  
+    public static void main(String[] args) {
+      int z = myMethod(5, 3);
+      System.out.println(z);
+    }
+  }
+  
