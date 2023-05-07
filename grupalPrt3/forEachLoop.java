02118c484123b7b6cc10bd7d1b5a297373b9450a
Dan1elMon <dchuramo@unsa.edu.pe>
2023-05-07 15:39:26 -0500

    ejercicio forEach.java

---
+++ grupalPrt3/forEachLoop.java
@@ -0,0 +1,9 @@
+public class forEachLook {
+    public static void main(String[] args) {
+        String[]carros={"Volvo", "BMW", "Ford", "Mazda"};
+        for(String i:carros){
+            System.out.println(i);
+            
+        }
+    }
+}
