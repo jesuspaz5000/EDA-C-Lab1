aef5b26e8f6ed8a3a05e68bf4772e99aeead9eda
Dan1elMon <dchuramo@unsa.edu.pe>
2023-05-07 15:47:09 -0500

    corrigiendo forBucleEj.java

--- grupalPrt3/forBucleEj.java
+++ grupalPrt3/forBucleEj.java
@@ -1,5 +1,8 @@
 public class forBucleEj {
-    for(int i = 0; i<=10; i=i+2){
-        System.out.println(i);
+    public static void main(String[] args) {
+        for(int i = 0; i<=10; i=i+2){
+            System.out.println(i);
+        }
     }
+    
 }
