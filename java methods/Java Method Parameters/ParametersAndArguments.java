34e8ccd5e4986e3c078acb463d5b3eeae14084be
jesuspaz <jesuspaz5000>
2023-05-07 12:58:31 -0500

    Parameters and Arguments

---
+++ java methods/Java Method Parameters/ParametersAndArguments.java
@@ -0,0 +1,12 @@
+public class ParametersAndArguments {
+    static void myMethod(String fname) {
+      System.out.println(fname + " Refsnes");
+    }
+  
+    public static void main(String[] args) {
+      myMethod("Liam");
+      myMethod("Jenny");
+      myMethod("Anja");
+    }
+  }
+  
