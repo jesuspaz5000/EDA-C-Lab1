dc323276907ba91b21fe21debccd450cc74730a5
xCristianx0604 <ccasso@unsa.edu.pe>
2023-05-07 15:56:33 -0500

    MATH JAVA

---
+++ la_clase_math/LA_CLASE_MATH.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package la_clase_math;
+import java.util.*;
+/**
+ *
+ * @author User
+ */
+public class LA_CLASE_MATH {
+
+    
+    public static void main(String[] args) {
+        System.out.println("LA CLASE MATH DE JAVA");
+        System.out.println(Math.max(5, 10)); //devuelve el numero maximo
+        System.out.println(Math.min(5, 10)); //devuelve el numero minimo
+        System.out.println(Math.sqrt(64)); //devuelve la raiz cuadrada
+        System.out.println(Math.abs(-4.7)); // devuelve el valor absoluto
+        System.out.println(Math.random());  // devuelve un numeor aleatorio entre 0 y 1
+    }
+    
+}
